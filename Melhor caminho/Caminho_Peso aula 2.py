cf6c22cdfaa17d8750db52e4461201f9360459a8
Gustavo <gsilvagon@gmail.com.br>
2018-05-03 14:40:49 -0300

    Algoritimo a * funfante.
    Algoritimo a estrelando funcionando para o problema do caixeiro viajante.

--- Melhor caminho/Caminho_Peso aula 2.py
+++ Melhor caminho/Caminho_Peso aula 2.py
@@ -2,105 +2,111 @@
 import pandas as pd
 import scipy.io as sp
 
-
+#Formando a tabela de distancia heuclidiana na mão
 distNome = ["Arad", "Bucharest", "Craiova", "Drobeta", "Eforie", "Fagaras", "Giurgi",
         "Hirsova", "Iasi", "Lugoj", "Mehadia", "Neamt", "Oradea", "Pitesti", "Rimnicu Vilcea",
-        "Sibiu", "Timisoara", "Urziceni", "Vaslui", "Zerid"]
+        "Sibiu", "Timisoara", "Urziceni", "Vaslui", "Zerind"];
 distQuant = [366, 0, 160, 242, 161, 176, 77, 151, 226, 244, 241, 234, 380, 100, 193, 253, 329,
-               80, 199, 374]
+               80, 199, 374];
+
+dist = pd.DataFrame(distQuant, distNome);
+#=======================================================================================
 
 explorado = []; #Inicia o conjunto explorado como vazio
 caminho = []; #Inicia o caminho como vazio
 fronteira = [];
+custo = 0;
 
 def estrela(mapa,ini,fim):
 
     G = 0
-    H = 0
+    H = getDistValue(ini)
     F = G + H 
-    node = [ini, G, H, F]
+    node = [ini, ini, G, H, F]
     custo = 0
-    #dist = pd.DataFrame(distQuant, distNome)
 
     if(node[0] == fim):
         print('O n de inicio ja é o nó de destino.');
         return
     
-    fronteira.append(node); #[Nó, Nó_Pai, Custo]
+    fronteira.append(node); #[Nó, NóPai, pesoCaminho, dist, soma]
 
     while(fronteira):
-
-        #ind = maisBarato(fronteira)
-        node = fronteira.pop(0) #Nó mais leve da fronteira.
-        if( node[0] == fim):
-            return solucao(node[0], ini)
+        
+        node = fronteira.pop(maisBarato(fronteira)) #Nó mais leve da fronteira.
         
         explorado.append(node)  #Adiciona no  explorados o nó analisado.
+        
+        if( node[0] == fim):
+            return solucao(ini)    
         
         tabelaPeso = mapa[mapa[node[0]]>0][node[0]]    #possiveis caminhos com respectivos pessos
         opcoes = list(tabelaPeso.index.values)         #opcoes com apenas seus nomes
 
         for filho in opcoes:    #Possiveis caminhos a seguir a partir do nó.
             
-            G = tabelaPeso[filho]
-            #H_F = dist[0]
-            filhoCompleto = [filho[0], node[0], filho[1]]  
+            G = tabelaPeso[filho];
+            H = getDistValue(filho);
+            F = G + H;
+            filhoCompleto = [filho, node[0], G, H, F]
             
-            if ( not explorado.count(filhoCompleto) > 0 ):# or not fronteira.count(filhoCompleto) > 0 :   #TODO: Nao esta verificando direito Explorado e Fronteira
+            if ( not isExplorado(filho) ):
                 
                 G_tempF = G + custo
                 
                 if( not fronteira.count(filhoCompleto) > 0 ):
                     
-                    fronteira.append([filho,node[0],node[2]])
+                    fronteira.append(filhoCompleto)
                     G_F = G_tempF
-                    F_F = G_F + H_F
+                    #F_F = G_F + H
+                    #custo += F_F
 
-                else:             
+                else: 
                     if( G_F > G_tempF ):
-                    
+                
                         G_F = G_tempF
-                        F_F = G_F + H_F
+                        #F_F = G_F + H
                     
     if(not fronteira):
-        caminho = -1
         custo = -1
         print("Não foi possível encontrar um caminho do nó de início ao nó de destino.")
         return
     return
 
-def maisBarato(fronteira):
+def maisBarato(fronteira):  #Get da fronteira com o pesso menor;
     
     ref = 1000000000000
     
-    for front in fronteira:
-        peso = front[len(front) - 1]
+    for k in range(0,len(fronteira)):
+        peso = fronteira[k][4]
         if ( peso < ref ):
-            maisBarato = front.index
-            #maisBarato = fronteira.pop(fronteira.index)
+            ref = peso
+            maisBarato = k
             
     return maisBarato
 
-def solucao(fim,ini):
+def solucao(ini):   #Mostra a rota das cidades e o custo total por passar por elas;
 
+    custoCaminho = 0
     menor = []
-    menor.append(explorado[len(explorado) - 1][0])
+    menor.append(explorado[len(explorado) - 1][0])  #Nó filho
     
-    proximo = explorado[len(explorado) - 1][0]
+    proximo = explorado[len(explorado) - 1][0]#Nó pai de origem
     
-    for n in range(len(explorado) - 1,0,-1):
+    for n in range(len(explorado) - 1,0,-1):    #Varrer todos as cidades exploradas
         if ( explorado[n][0] == proximo ):
             menor.append(explorado[n][1])
             proximo = explorado[n][1]
-        if ( proximo == ini ):
+            custoCaminho += explorado[n][2]
+        if ( proximo == ini ):  #Encontrado o destino
             break
-    for p in range(len(menor) - 1,-1,-1):
+    
+    for p in range(len(menor) - 1,-1,-1):   #Ajusta o caminho na sequencia
         caminho.append(menor[p])    
-    caminho.append(fim)
-        
-    return print(caminho)
 
-def getMap():
+    return print(caminho, '\n\nCusto do Caminho:',custoCaminho )
+
+def getMap():   #Gera o DataFrame do mapa para manipulação
 
     data = sp.loadmat('data_Romenia.mat')
     
@@ -112,14 +118,23 @@ def getMap():
 
     return(df)
 
-def main():
-    #mapa = getMap()
-    dist = pd.DataFrame(distQuant, distNome)
-    tt = dist.index("Arad")
-    #estrela(mapa,'Arad','Bucharest')
-    #H_F = 
-    print(tt)
+def getDistValue(cidade):   #Valor na tabela dist
+    return dist.loc[cidade][0]
 
-#Inicio do programa
+def isExplorado(filho): #Verifica se a cidade ja foi explorada
+    isEx = False;
+    
+    for ex in explorado:
+        if (ex[0] == filho):
+            isEx = True;
+            break;
+    return isEx
+
+
+def main():
+    mapa = getMap()
+    estrela(mapa,'Lugoj','Bucharest')
+
+#Inicio do programa==========================================
     
 main()
