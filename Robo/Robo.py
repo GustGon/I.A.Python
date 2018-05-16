796b5c55e401384067ab3cf4f0019f57625ce65d
Gustavo <gsilvagon@gmail.com.br>
2018-05-16 16:18:34 -0300

    Começo da fronteira com alguns bugs.
    Esta reconhecendo a fronteira porem em lugares aleatorios... e esta pintando os quadrados.

--- Robo/Robo.py
+++ Robo/Robo.py
@@ -14,21 +14,31 @@ fronteira = [];
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
+BLACK_GREEN = (0, 125, 0)
+LIGTH_GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
 COMECO = 1
 FIM = 2
 OBSTACULO = 3
+FRONTEIRA = 4
+EXPLORADO = 5
  
 # This sets the WIDTH and HEIGHT of each grid location
 WIDTH = 20
 HEIGHT = 20
+
  
 # This sets the margin between each cell
 MARGIN = 5
  
+#Botoes
+LEFT_CLICK = 1
+MIDLE_CLICK = 1
+RIGHT_CLICK = 3
+
+
 # Create a 2 dimensional array. A two dimensional
 # array is simply a list of lists.
 grid = []
@@ -38,82 +48,11 @@ for row in range(10):
     grid.append([])
     for column in range(10):
         grid[row].append(0)  # Append a cell
+    
+    # Set row 1, cell 5 to one. (Remember rows and
+    # column numbers start at zero.)
+    #grid[1][5] = 1
  
-# Set row 1, cell 5 to one. (Remember rows and
-# column numbers start at zero.)
-#grid[1][5] = 1
- 
-# Initialize pygame
-pygame.init()
- 
-# Set the HEIGHT and WIDTH of the screen
-WINDOW_SIZE = [255, 255]
-screen = pygame.display.set_mode(WINDOW_SIZE)
- 
-# Set title of screen
-pygame.display.set_caption("Array Backed Grid")
- 
-# Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-
-#Contagem para saber quando é o click de começo e fim
-count = 1
-# -------- Main Program Loop -----------
-while not done:
-    for event in pygame.event.get():  # User did something
-        if event.type == pygame.QUIT:  # If user clicked close
-            done = True  # Flag that we are done so we exit this loop
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # User clicks the mouse. Get the position
-            pos = pygame.mouse.get_pos()
-            # Change the x/y screen coordinates to grid coordinates
-            column = pos[0] // (WIDTH + MARGIN)
-            row = pos[1] // (HEIGHT + MARGIN)
-                
-            # Set that location to one
-            grid[row][column] = count
-            print("Click ", pos, "Grid coordinates: ", row, column)
-            if ( COMECO == count ):
-                ini = [column,row]
-                count += 1
-            elif ( FIM == count ):
-                end = [column,row]
-                count += 1
-
- 
-    # Set the screen background
-    screen.fill(BLACK)
- 
-    # Draw the grid
-    for row in range(10):
-        for column in range(10):
-            color = WHITE
-            if grid[row][column] == COMECO:
-                color = BLUE
-            elif grid[row][column] == FIM:
-                color = BLACK
-            elif grid[row][column] == OBSTACULO:
-                color = RED
-            pygame.draw.rect(screen,
-                             color,
-                             [(MARGIN + WIDTH) * column + MARGIN,
-                              (MARGIN + HEIGHT) * row + MARGIN,
-                              WIDTH,
-                              HEIGHT])
- 
-    # Limit to 60 frames per second
-    clock.tick(60)
- 
-    # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
-# Be IDLE friendly. If you forget this line, the program will 'hang'
-# on exit.
-pygame.quit()
-
 
 def encontraCaminho(ini, end):
     
@@ -133,29 +72,28 @@ def encontraCaminho(ini, end):
         
         node = fronteira.pop(maisBarato(fronteira)) #Nó mais leve da fronteira.
         
+        opcoes = getAcoes(node)         #opcoes com apenas seus nomes
+        
         explorado.append(node)  #Adiciona no  explorados o nó analisado.
         
         if( node[0] == end):
             return solucao(ini)    
-        
-        tabelaPeso = mapa[mapa[node[0]]>0][node[0]]    #possiveis caminhos com respectivos pessos
-        opcoes = list(tabelaPeso.index.values)         #opcoes com apenas seus nomes
 
         for filho in opcoes:    #Possiveis caminhos a seguir a partir do nó.
             
             G = 1;
-            H = getDistValue(filho);
+            H = getDistValue(filho, end);
             F = G + H;
             filhoCompleto = [filho, node[0], G, H, F]
-            colum = 2
-            row = 2
+            column = filho[0]
+            row = filho[1]
             
-            if ( not isExplorado(filho) ):
+            if ( not isExplorado(filhoCompleto) ):
                 
                 G_tempF = G + custo
             
             elif( ( not fronteira.count(filhoCompleto) > 0 ) and
-                 not( 0 > colum > WIDTH or 0 > row > HEIGHT ) ):
+                 not( 0 > column > WIDTH or 0 > row > HEIGHT ) ):
                     
                 fronteira.append(filhoCompleto)
                 G_F = G_tempF
@@ -168,6 +106,24 @@ def encontraCaminho(ini, end):
                 #F_F = G_F + H
     
     return
+
+def getAcoes(local):
+    
+    opcoes = []
+    
+    opcoes.append([local[0][0] + 1, local[1][1] + 1]);
+    opcoes.append([local[0][0] + 0, local[1][1] + 1]);
+    opcoes.append([local[0][0] + 1, local[1][1] - 1]);
+    opcoes.append([local[0][0] + 1, local[1][1] + 0]);
+    opcoes.append([local[0][0] - 1, local[1][1] - 1]);
+    opcoes.append([local[0][0] - 0, local[1][1] - 1]);
+    opcoes.append([local[0][0] - 1, local[1][1] + 1]);
+    opcoes.append([local[0][0] - 1, local[1][1] - 0]);
+    
+    for op in opcoes:
+        grid[op[0]][op[1]] = FRONTEIRA
+    
+    return opcoes
 
 def isExplorado(filho): #Verifica se a cidade ja foi explorada
     isEx = False;
@@ -211,8 +167,96 @@ def solucao(ini):   #Mostra a rota das cidades e o custo total por passar por el
 
     return print(caminho, '\n\nCusto do Caminho:',custoCaminho )
 
-def getDistValue(ini, end):   #Valor na tabela dist
+def getDistValue(ini, end):
+    #Heuclidiana
     deltaColun = ini[0] - end[0]
     deltaRow = ini[1] - end[1]
     dist = (deltaColun**2 + deltaRow**2)**(1/2)
     return dist
+
+def main():
+
+    # Initialize pygame
+    pygame.init()
+     
+    # Set the HEIGHT and WIDTH of the screen
+    WINDOW_SIZE = [255, 255]
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+     
+    # Set title of screen
+    pygame.display.set_caption("Array Backed Grid")
+     
+    # Loop until the user clicks the close button.
+    done = False
+     
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+    
+    #Contagem para saber quando é o click de começo e fim
+    count = 1
+    # -------- Main Program Loop -----------
+    while not done:
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                done = True  # Flag that we are done so we exit this loop
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                print( event.button )
+                if ( event.button == LEFT_CLICK ):
+                    # User clicks the mouse. Get the position
+                    pos = pygame.mouse.get_pos()
+                    # Change the x/y screen coordinates to grid coordinates
+                    column = pos[0] // (WIDTH + MARGIN)
+                    row = pos[1] // (HEIGHT + MARGIN)
+                    if ( column >= 10 ):
+                        break;
+                    # Set that location to one
+                    grid[row][column] = count
+                    print("Click ", pos, "Grid coordinates: ", row, column)
+                    if ( COMECO == count ):
+                        grid[row][column] = COMECO;
+                        ini = [column,row]
+                        count += 1
+                    elif ( FIM == count ):
+                        grid[row][column] = FIM;
+                        end = [column,row]
+                        count += 1
+                elif ( event.button == RIGHT_CLICK ):
+                    encontraCaminho(ini, end);
+    
+        # Set the screen background
+        screen.fill(BLACK)
+     
+        # Draw the grid
+        for row in range(10):
+            for column in range(10):
+                color = WHITE
+                if grid[row][column] == COMECO:
+                    color = BLUE
+                elif grid[row][column] == FIM:
+                    color = BLACK
+                elif grid[row][column] == OBSTACULO:
+                    color = RED
+                elif grid[row][column] == FRONTEIRA:
+                    color = BLACK_GREEN
+                elif grid[row][column] == EXPLORADO:
+                    color = LIGTH_GREEN
+                pygame.draw.rect(screen,
+                                 color,
+                                 [(MARGIN + WIDTH) * column + MARGIN,
+                                  (MARGIN + HEIGHT) * row + MARGIN,
+                                  WIDTH,
+                                  HEIGHT])
+     
+        # Limit to 60 frames per second
+        clock.tick(60)
+     
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+     
+    # Be IDLE friendly. If you forget this line, the program will 'hang'
+    # on exit.
+    pygame.quit()
+    return
+
+#INIT programa
+main();
