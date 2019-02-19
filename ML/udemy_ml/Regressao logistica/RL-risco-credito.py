0baead31abed1245c435966a25e710f0dd09bc72
Gustavo <gsilvagon@gmail.com.br>
2019-02-19 16:51:36 -0300

    Alogoritimo de Regresao logistica (nao terminado)

--- ML/udemy_ml/Regressao logistica/RL-risco-credito.py
+++ ML/udemy_ml/Regressao logistica/RL-risco-credito.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-base = pd.read_csv('../csv/risco-credito.csv')
+base = pd.read_csv('../csv/risco-credito2.csv')
 previsores = base.iloc[:, 0:4].values
 classe = base.iloc[:, 4].values
 
@@ -11,20 +11,14 @@ previsores[:, 1] = labelenconder.fit_transform(previsores[:, 1])
 previsores[:, 2] = labelenconder.fit_transform(previsores[:, 2])
 previsores[:, 3] = labelenconder.fit_transform(previsores[:, 3])
 
-from sklearn.tree import DecisionTreeClassifier, export
-classificador = DecisionTreeClassifier(criterion='entropy')
-classificador.fit(previsores, classe)
-
-export.export_graphviz(classificador,
-                       out_file='arvore.dot',
-                       feature_names=['historia', 'divida', 'garantia', 'renda'],
-                       filled=True,
-                       leaves_parallel=True)
+from sklearn.linear_model import logistic_regression_path
+classificador = logistic_regression_path(X=previsores, y=classe)
 
 # historia boa, divida alta, garantias nenhuma, renda > 35
 # historia ruim, divida alta, garantias adequada, renda < 35
 resultado = classificador.predict([[0, 0, 1, 2], [3, 0, 0, 0]])
 
-print(classificador.feature_importances_)
-print(classificador.classes_)
-print(resultado)
+print(classificador)
+# print(classificador.class_count_)
+# print(classificador.class_prior_)
+# print(resultado)
